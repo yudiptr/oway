29ab5ee33c2ee341f77ca09e60dded09409b4229
Yudi Putra Sabri <yudi.sabri@akseleran.com>
2025-02-03 14:19:21 +0700

    fix: improve the delete mail test

--- app/src/test/java/org/example/DeleteMailTest.java
+++ app/src/test/java/org/example/DeleteMailTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,8 +15,6 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.List;
 import java.util.logging.Logger;
-// For selenium version < 4, use this import
-// import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DeleteMailTest {
     private WebDriver driver;
@@ -25,73 +23,72 @@ public class DeleteMailTest {
 
     @BeforeTest
     public void setUp() {
-        // For Chrome < 112
-        // WebDriverManager.chromedriver().setup();
-
-        // For Chrome > 112, path to chromedriver for specific version, download manual through chrome
         logger.info("Setting up the WebDriver");
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
-
         options.addArguments("--remote-allow-origins=*");
-
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(60));
     }
 
     @Test
-    public void gmailLoginAndFetchUnreadEmail() {
-        try {
+    public void testDeleteUnreadEmail() {
+        loginToGmail("popoyudi333@gmail.com", "yudisabri123");
+        deleteLatestUnreadEmail();
+    }
 
-            // Login Gmail
-            driver.get("https://mail.google.com/");
-            logger.info("Navigated to Gmail login page");
+    private void loginToGmail(String email, String password) {
+        driver.get("https://mail.google.com/");
+        logger.info("Navigated to Gmail login page");
 
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("identifierId")));
+        Assert.assertNotNull(emailField, "Email field not found!");
+        emailField.sendKeys(email);
+        driver.findElement(By.id("identifierNext")).click();
+        logger.info("Entered email and clicked next");
 
-            WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("identifierId")));
-            emailField.sendKeys("popoyudi333@gmail.com");
-            driver.findElement(By.id("identifierNext")).click();
-            logger.info("Entered email and clicked next");
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Passwd")));
+        Assert.assertNotNull(passwordField, "Password field not found!");
+        passwordField.sendKeys(password);
+        driver.findElement(By.id("passwordNext")).click();
+        logger.info("Entered password and clicked next");
 
-            WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Passwd")));
-            passwordField.sendKeys("yudisabri123");
-            driver.findElement(By.id("passwordNext")).click();
-            logger.info("Entered password and clicked next");
+        // Verify successful login by checking the page title
+        wait.until(ExpectedConditions.titleContains("Kotak Masuk"));
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(pageTitle.contains("Kotak Masuk"), "Login failed! Expected page title to contain 'Kotak Masuk', but got: " + pageTitle);
+        logger.info("Successfully logged into Gmail");
+    }
 
-            // Login Success, Navigate to Mail
-            wait.until(ExpectedConditions.titleContains("Kotak Masuk"));
-            logger.info("Successfully logged into Gmail");
-            
-            // Getting latest unread email
-            List<WebElement> unreadEmails = driver.findElements(By.cssSelector(".zA.zE"));
-            if (!unreadEmails.isEmpty()) {
-                WebElement lastUnreadEmail = unreadEmails.get(0);
-                String emailTitle = lastUnreadEmail.findElement(By.cssSelector(".bog")).getText();
-                logger.info("Last unread email title: " + emailTitle);
+    private void deleteLatestUnreadEmail() {
+        List<WebElement> unreadEmails = driver.findElements(By.cssSelector(".zA.zE"));
+        Assert.assertFalse(unreadEmails.isEmpty(), "No unread emails found to delete.");
 
-                WebElement emailCheckbox = lastUnreadEmail.findElement(By.cssSelector(".T-Jo"));
-                emailCheckbox.click();
-                logger.info("Selected the unread email");
+        WebElement lastUnreadEmail = unreadEmails.get(0);
+        String emailTitle = lastUnreadEmail.findElement(By.cssSelector(".bog")).getText();
+        logger.info("Last unread email title: " + emailTitle);
 
-                WebElement deleteButton = wait
-                        .until(ExpectedConditions.elementToBeClickable(By.cssSelector(".nX.T-I-ax7")));
-                deleteButton.click();
-                logger.info("Deleted the selected email");
+        WebElement emailCheckbox = lastUnreadEmail.findElement(By.cssSelector(".T-Jo"));
+        Assert.assertNotNull(emailCheckbox, "Checkbox for selecting email not found!");
+        emailCheckbox.click();
+        logger.info("Selected the unread email");
 
-                wait.until(ExpectedConditions.invisibilityOf(lastUnreadEmail));
-                logger.info("Email successfully deleted");
-            } else {
-                logger.info("No unread emails found.");
-            }
-        } catch (Exception e) {
-            logger.severe("Error during Gmail login, fetch, or delete: " + e.getMessage());
-        }
+        WebElement deleteButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".nX.T-I-ax7")));
+        Assert.assertNotNull(deleteButton, "Delete button not found!");
+        deleteButton.click();
+        logger.info("Deleted the selected email");
+
+        // Wait until the email disappears and verify it is deleted
+        wait.until(ExpectedConditions.invisibilityOf(lastUnreadEmail));
+        List<WebElement> remainingUnreadEmails = driver.findElements(By.cssSelector(".zA.zE"));
+        Assert.assertFalse(remainingUnreadEmails.contains(lastUnreadEmail), "Email was not successfully deleted.");
+        logger.info("Email successfully deleted");
     }
 
     @AfterTest
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+            logger.info("Closed the browser");
         }
     }
 }
